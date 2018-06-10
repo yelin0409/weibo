e1a928292659eb94bcffdb70820587cf00cdd0b6
yelin0409 <3263251963@qq.com>
2018-06-10 23:54:39 +0800

    first commit

---
+++ src/main/java/weibo/Weibo.java
@@ -0,0 +1,15 @@
+package weibo;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class Weibo {
+
+    private Configuration conf = null;
+
+    private static final byte[] TABLE_CON
+
+    public static void main(String[] args) {
+
+    }
+
+}
